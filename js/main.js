58554dee5e46f7a233ca2f8f38d5c5fe15dc5f15
Victor Luo <victorluo@MacBook-Air.local>
2020-03-27 16:01:10 -0700

    styled buttons

--- js/main.js
+++ js/main.js
@@ -3,26 +3,14 @@ let currentValue = 0;
 $(".counter").text(currentValue)
 
 
-$(".adder").hover(function(){
-    $(this).css("color", "black");
-    }, function(){
-    $(this).css("color", "hitw");
-  });
-
-$(".subtractor").hover(function(){
-    $(this).css("color", "black");
-    }, function(){
-    $(this).css("color", "hitw");
-  });
-
 
 $(".adder").on('click', function(){
     currentValue = currentValue + parseInt($(".input").val());
     $(".counter").text(currentValue);
-})
+});
 
 $(".subtractor").on('click', function(){
     currentValue = currentValue - parseInt($(".input").val());
     $(".counter").text(currentValue);
-})
+});
 
